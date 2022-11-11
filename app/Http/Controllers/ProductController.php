256148f57494fe583549db88ccc755444d39d940
Mahmudul Hasan <mahmudul.ruet12@gmail.com>
2022-11-11 21:45:43 +0100

    sanctum package installed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -70,4 +70,9 @@ class ProductController extends Controller
     {
         return Product::destroy($id);
     }
+
+    public function search($name)
+    {
+        return Product::where('name', 'like', '%'.$name.'%')->get();
+    }
 }
